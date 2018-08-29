362d40b11f0beb4d8542e755c4ccb77234f79703
ammo0110 <amoghgaur@tuta.io>
2018-08-29 22:52:55 +0530

    Main Menu Foundation Stone

--- app/src/main/java/xyz/ammo/vocabularybuilder/HomeActivity.java
+++ app/src/main/java/xyz/ammo/vocabularybuilder/HomeActivity.java
@@ -1,7 +1,9 @@
 package xyz.ammo.vocabularybuilder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void launchGame(View v) {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
     }
 }
