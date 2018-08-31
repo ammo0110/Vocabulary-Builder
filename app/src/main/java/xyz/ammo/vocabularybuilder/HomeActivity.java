fbbad6b868de1731121debfaba8b908d69b24823
ammo0110 <amoghgaur@tuta.io>
2018-08-31 08:54:15 +0530

    Database Handling Initiated

--- app/src/main/java/xyz/ammo/vocabularybuilder/HomeActivity.java
+++ app/src/main/java/xyz/ammo/vocabularybuilder/HomeActivity.java
@@ -3,17 +3,51 @@ package xyz.ammo.vocabularybuilder;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import butterknife.OnClick;
+import butterknife.ButterKnife;
 
 public class HomeActivity extends AppCompatActivity {
+
+    public static final String TAG = "MyHomeActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ButterKnife.bind(this);
+
+        String localPath = getFilesDir() + "/" + "PrimaryWords.db";
+        File dbFile = new File(localPath);
+        Log.d(TAG, "Primary database location: " + localPath);
+        if(!dbFile.exists()) {
+            Log.d(TAG, "Going to copy assets/databases/Words.db to local space");
+            // Copy database from assets to local directory for the first time
+            try {
+                InputStream is = getAssets().open("databases/Words.db");
+                FileOutputStream os = new FileOutputStream(dbFile);
+                byte[] buf = new byte[1024];
+                while(is.read(buf) > 0) {
+                    os.write(buf);
+                }
+                os.flush();
+                is.close();
+                os.close();
+                Log.d(TAG, "assets/databases/Words.db copied");
+            }
+            catch(IOException ex) {
+                Log.e(TAG, "Exception occurred: " + ex.getMessage());
+            }
+        }
     }
 
-    public void launchGame(View v) {
+    @OnClick(R.id.choice1) void launchGame() {
         Intent intent = new Intent(this, GameActivity.class);
         startActivity(intent);
     }
---
+++ app/src/main/java/xyz/ammo/vocabularybuilder/storage/WordDBOpenHelper.java
@@ -0,0 +1,26 @@
+package xyz.ammo.vocabularybuilder.storage;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class WordDBOpenHelper extends SQLiteOpenHelper {
+
+    public static final int DATABASE_VERSION = 1;
+    public static final String TAG = "MyWordDBOpenHelper";
+
+    public WordDBOpenHelper(Context context, String databaseName) {
+        super(context, databaseName, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        Log.d(TAG, "onCreate Invoked");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.d(TAG, "onUpgrade Invoked");
+    }
+}
